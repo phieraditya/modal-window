624115e6c3e09f993c9989976d54ebc34b394ee6
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2021-12-24 16:37:47 +0100

    refactor code
    -add closeModal function
    -add openModal function

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  btnOpenModal[i].addEventListener("click", openModal);
 
-btnCloseModal.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
-
-overlay.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
