faf429cd5824b7c8995948b208c2244d59e66690
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2021-12-24 16:55:45 +0100

    minor code refactor

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    if (!modal.classList.contains("hidden")) closeModal();
-  }
+  if (event.key === "Escape" && !modal.classList.contains("hidden"))
+    closeModal();
 });
